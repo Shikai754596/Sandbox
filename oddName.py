671e42469ea6f02e9db98105678a11766559bc4d
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-08-05 11:09:18 +0800

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,9 @@
 """ Huang Shikai """
+user_name = input("Please enter your name: ")
+
+while user_name.isspace() is True or user_name == "":
+    user_name = input("Can not be blank. Please enter your name: ")
+
+for w in user_name:
+    print(w, end=" ")
+
